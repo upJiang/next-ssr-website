e7002c829a897891ffce22dd6cc2511539c55e4d
江俊锋 <jiangjf04@vanke.com>
2023-02-16 22:14:54 +0800

    feat(init): 更新

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d35372b5b0e88e8e"; }
+/******/ 		__webpack_require__.h = function() { return "50892c3cbd6c0cfa"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
-const path = require("path");
+const path = require('path')
 
 module.exports = {
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
   reactStrictMode: true,
   swcMinify: true,
-  webpack: (config) => {
+  webpack: config => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      "@": path.resolve(__dirname),
-    };
-    return config;
+      '@': path.resolve(__dirname),
+    }
+    return config
   },
-};
+}
